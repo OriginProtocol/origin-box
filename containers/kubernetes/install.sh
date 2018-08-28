579d06a618663dcfb4170459d04bbb4f947296b5
tomlinton <tlinton@dockyard.nz>
2018-08-28 22:15:39 +1200

    Add missing slash

--- containers/kubernetes/install.sh
+++ containers/kubernetes/install.sh
@@ -1,6 +1,6 @@
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule \
-	--clusterrole=cluster-admin
+	--clusterrole=cluster-admin \
 	--serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy \
 	-p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
