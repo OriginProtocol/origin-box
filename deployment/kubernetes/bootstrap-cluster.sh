51f38da4b56f81bc2418eba34f0294593483caec
tomlinton <tlinton@dockyard.nz>
2018-09-04 18:06:34 +1200

    Update names of ingress deployments

--- deployment/kubernetes/bootstrap-cluster.sh
+++ deployment/kubernetes/bootstrap-cluster.sh
@@ -18,11 +18,13 @@ kubectl create -f namespaces.yaml
 
 sleep 10
 
+# TODO try and do this via requirements.yaml for the Origin chart
+
 # Install cert-manager for issuing SSL certificates through LetsEncrypt
 helm install stable/cert-manager --name cert-manager \
 	--namespace kube-system	\
 	--set rbac.create=true \
-	--set ingressShim.defaultIssuerName=letsencrypt-clusterissuer \
+	--set ingressShim.defaultIssuerName=letsencrypt-staging \
 	--set ingressShim.defaultIssuerKind=ClusterIssuer
 
 # The nginx-ingress controllers need separate names because helm requires
@@ -30,19 +32,19 @@ helm install stable/cert-manager --name cert-manager \
 # See https://github.com/helm/helm/issues/2060
 
 # Install nginx ingress for development
-helm install stable/nginx-ingress --name development \
+helm install stable/nginx-ingress --name development-ingress \
 	--namespace development \
 	--set rbac.create=true \
 	--set controller.service.loadBalancerIP="35.233.140.121"
 
 # Install nginx ingress for staging
-helm install stable/nginx-ingress --name staging \
+helm install stable/nginx-ingress --name staging-ingress \
 	--namespace staging \
 	--set rbac.create=true \
 	--set controller.service.loadBalancerIP="35.197.88.39"
 
 # Install nginx ingress for production
-helm install stable/nginx-ingress --name production \
+helm install stable/nginx-ingress --name production-ingress \
 	--namespace production \
 	--set rbac.create=true \
 	--set controller.service.loadBalancerIP="35.203.166.86"
