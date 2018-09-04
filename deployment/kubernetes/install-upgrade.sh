ad282ece89eb568dbac3126adaac2e6b3342a7e5
tomlinton <tlinton@dockyard.nz>
2018-09-05 09:05:52 +1200

    Rearrange chart values directory

--- deployment/kubernetes/install-upgrade.sh
+++ deployment/kubernetes/install-upgrade.sh
@@ -3,6 +3,6 @@
 helm upgrade $1 \
 	origin \
 	-i \
-	-f origin/values/values.yaml \
-	-f origin/values/values-$1.yaml \
+	-f origin/values.yaml \
+	-f origin/environments/values-$1.yaml \
 	--namespace $1
