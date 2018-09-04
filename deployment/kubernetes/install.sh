c802347e32d70dafa9f298c15d5c13c784a7225a
tomlinton <tlinton@dockyard.nz>
2018-09-04 14:37:46 +1200

    Restyle install script

--- deployment/kubernetes/install.sh
+++ deployment/kubernetes/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-helm install origin -f origin/values.yaml -f origin/values-$1.yaml --namespace $1
+helm install origin \
+	-f origin/values.yaml \
+	-f origin/values-$1.yaml \
+	--namespace $1
