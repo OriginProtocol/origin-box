19840a6e236700358f06a48b4003d746075e8329
tomlinton <tlinton@dockyard.nz>
2018-09-06 22:32:27 +1200

    Secret handling

--- deployment/kubernetes/upgrade.sh
+++ deployment/kubernetes/upgrade.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! -f origin/values-secret.yaml ]; then
+    echo -e "\033[31mNo secrets file found at origin/values-secret.yaml\033[0m"
+    echo -e ""
+    echo -e "It must be populated with the following configuration items:"
+    echo -e "\t envKeyBridge - EnvKey key for use with origin-bridge"
+    echo
+    exit
+fi
+
 helm upgrade $1 \
 	origin \
 	-i \
