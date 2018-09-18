c5953f2e21ce477d17f629b86affddac07c4dcf4
tomlinton <tlinton@dockyard.nz>
2018-09-18 17:08:30 +1200

    Secrets handling with SOPS

--- deployment/kubernetes/sync-chart.sh
+++ deployment/kubernetes/sync-chart.sh
@@ -5,13 +5,10 @@ if [ "$1" != "dev" ] && [ "$1" != "staging" ] && [ "$1" != "prod" ]; then
     exit
 fi
 
-if [ ! -f values/secret.yaml ]; then
-    echo -e "\033[31mNo secrets file found at values/secret.yaml\033[0m"
+if [ ! -f values/secrets-$1.yaml ]; then
+    echo -e "\033[31mNo secrets file found at values/secrets-$1.yaml\033[0m"
     echo -e ""
-    echo -e "It must be populated with the following configuration items:"
-    echo -e "\t bridgeEnvKey - EnvKey key for use with origin-bridge"
-    echo -e "\t gethAccountPrivateKey - Private key for account on Origin Ethereum network"
-    echo -e "\t gethAccountSecret - Secret for account on Origin Ethereum network"
+    echo -e "Did you forget to decrypt it? :)"
     exit
 fi
 
@@ -19,6 +16,6 @@ helm upgrade $1 \
 	chart \
 	-i \
 	-f chart/values.yaml \
-	-f values/$1.yaml \
-	-f values/secret.yaml \
+	-f values/values-$1.yaml \
+	-f values/secrets-$1.yaml \
 	--namespace $1
